5f697fd84a97dc5aebb5e03d6b3e8c9a1f4976df
f0xyx0f <alisaka4anova@gmail.com>
2024-08-29 21:00:02 +0300

    *настроила систему сохранения и меню уровней

---
+++ Pair_Rush/Assets/Scripts/Level_script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System;
+
+public class Level_script : MonoBehaviour
+{
+    [SerializeField] int n;
+    [SerializeField] GameObject[] marks = new GameObject[3];
+    private void Start()
+    {
+        SaveData.LoadGame();
+        marksActive(SaveData.ActiveLevels[n]);
+    }
+
+    private void marksActive(int m)
+    {
+        marks[m].SetActive(true);
+        foreach (GameObject go in marks.Where(g => Array.IndexOf(marks, g) != m))
+            go.SetActive(false);
+    }
+}
---
+++ Pair_Rush/Assets/Scripts/SaveData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveData
+{
+    public static int[] ActiveLevels;
+    public static void SaveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath
+          + "/MySaveData.dat");
+        Data data = new Data();
+        data.ActiveLevels = ActiveLevels;
+        bf.Serialize(file, data);
+        file.Close();
+    }
+    public static void LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath
+          + "/MySaveData.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file =
+              File.Open(Application.persistentDataPath
+              + "/MySaveData.dat", FileMode.Open);
+            Data data = (Data)bf.Deserialize(file);
+            file.Close();
+            ActiveLevels = data.ActiveLevels;
+            Debug.Log("Game data loaded!");
+        }
+        else
+        {
+            ActiveLevels = new int[9];
+            for (int i = 1; i < 9; ++i)
+                ActiveLevels[i] = 0;
+            ActiveLevels[0] = 1;
+        }
+    }
+    public static void ResetData()
+    {
+        if (File.Exists(Application.persistentDataPath
+          + "/MySaveData.dat"))
+        {
+            File.Delete(Application.persistentDataPath
+              + "/MySaveData.dat");
+        }
+    }
+}
+
+[Serializable]
+class Data
+{
+    public int[] ActiveLevels;
+}
